5cadee3894a09dcb828b34d6096dab84b81a7c2f
Thony Nava <thonynava@Thonys-MacBook-Pro.local>
2020-10-13 21:06:15 +0200

    Level 100

--- script.js
+++ script.js
@@ -5,8 +5,26 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  // const rootElem = document.getElementById("root");
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  console.log(episodeList);
+  episodeList.forEach((obj) => {
+    console.log(obj);
+    let div = document.createElement("div");
+    div.classList = "col-lg-4 p-2";
+    div.innerHTML = `
+    <div class="card w-100 h-100">
+      <img class="card-img-top rounded"
+      src="${obj.image.original}"
+      alt="image of episode ${obj.name}"/>
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title">${obj.name} | S${obj.season}E${obj.number}</h5>
+        <p class="card-text">${obj.summary}</p>
+        <a href="${obj.url}" target="_blank" class="btn btn-info mt-auto">More info</a>
+      </div>
+    </div>`;
+    document.getElementById("infoBoxes").appendChild(div);
+  });
 }
 
 window.onload = setup;
