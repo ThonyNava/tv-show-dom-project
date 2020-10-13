8a2e15432b3d8026ab57908cde910c93486d948a
Thony Nava <thonynava@Thonys-MacBook-Pro.local>
2020-10-13 22:07:59 +0200

    v2 beta

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
 
-function makePageForEpisodes(episodeList) {
-  // const rootElem = document.getElementById("root");
-  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  console.log(episodeList);
-  episodeList.forEach((obj) => {
-    console.log(obj);
-    let div = document.createElement("div");
-    div.classList = "col-lg-4 p-2";
-    div.innerHTML = `
-    <div class="card w-100 h-100">
-      <img class="card-img-top rounded"
-      src="${obj.image.original}"
-      alt="image of episode ${obj.name}"/>
-      <div class="card-body d-flex flex-column">
-        <h5 class="card-title">${obj.name} | S${obj.season}E${obj.number}</h5>
-        <p class="card-text">${obj.summary}</p>
-        <a href="${obj.url}" target="_blank" class="btn btn-info mt-auto">More info</a>
-      </div>
-    </div>`;
-    document.getElementById("infoBoxes").appendChild(div);
-  });
-}
+const allEpisodes = getAllEpisodes();
+let button = document.querySelector("button");
+let search = document.querySelector("input");
+let query;
+let gettingValue = () => {
+  query = search.value;
+};
 
-window.onload = setup;
+button.addEventListener("click", function () {
+  let api = "http://api.tvmaze.com/search/shows?q=";
+  let url = api + query;
+  console.log(url);
+  fetch(url)
+    .then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw new Error(
+          `Encountered something unexpected: ${response.status} ${response.statusText}`
+        );
+      }
+    })
+    .then((jsonResponse) => {
+      console.log(jsonResponse);
+      jsonResponse.forEach((obj) => {
+        let show = obj.show;
+        let div = document.createElement("div");
+        div.classList = "col-lg-4 p-2";
+        div.innerHTML = `
+          <div class="card w-100 h-100">
+            <img class="card-img-top rounded"
+            src="${show.image.original}"
+            alt="image of episode ${show.name}"/>
+            <div class="card-body d-flex flex-column">
+              <h5 class="card-title">${show.name} | S${show.season}E${show.number}</h5>
+              <p class="card-text">${show.summary}</p>
+              <a href="${show.url}" target="_blank" class="btn btn-info mt-auto">More info</a>
+            </div>
+          </div>`;
+        document.getElementById("infoBoxes").appendChild(div);
+      });
+    })
+    .catch((error) => {
+      // Handle the error
+      console.log(error);
+    });
+});
